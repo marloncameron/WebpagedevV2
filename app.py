17d6800a882ddb177109cd321f1fcc6fff56c8c1
marloncameron <68697926+marloncameron@users.noreply.github.com>
2024-03-13 15:30:45 +0000

    add technician and css

--- app.py
+++ app.py
@@ -2,9 +2,50 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOB = [
+  {
+    'id': 1,
+    'title': 'Conuntry 1',
+    'Technician 1': 'Name 1',
+    'Technician 2': 'Name 2',
+    'Technician 3': 'Name 3',
+    'Technician 4': 'Name 4',
+    
+  },
+  {
+    'id': 2,
+    'title': 'Conuntry 2',
+    'Technician 1': 'Name 1',
+    'Technician 2': 'Name 2',
+    'Technician 3': 'Name 3',
+    'Technician 4': 'Name 4',
+
+  },
+  {    
+    'id': 3,
+    'title': 'Conuntry 3',
+    'Technician 1': 'Name 1',
+    'Technician 2': 'Name 2',
+    'Technician 3': 'Name 3',
+    'Technician 4': 'Name 4',
+    
+  },
+  {
+    'id': 4,
+    'title': 'Conuntry 4',
+    'Technician 1': 'Name 1',
+    'Technician 2': 'Name 2',
+    'Technician 3': 'Name 3',
+    'Technician 4': 'Name 4',
+    
+  }
+]
+
+
+
 @app.route("/")
 def ITWEBsa():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOB)
 
 
 
